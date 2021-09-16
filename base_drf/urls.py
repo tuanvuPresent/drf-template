627caa4d3af29923499769c22f4e78690cb79295
tuanvuPresent <55792941+tuanvuPresent@users.noreply.github.com>
2021-09-16 19:53:40 +0700

    Update urls.py

--- base_drf/urls.py
+++ base_drf/urls.py
@@ -19,7 +19,14 @@ from django.contrib import admin
 from drf_yasg.views import get_schema_view
 from drf_yasg import openapi
 from rest_framework import permissions
+from drf_yasg.generators import OpenAPISchemaGenerator
 
+class BothHttpAndHttpsSchemaGenerator(OpenAPISchemaGenerator): 
+    def get_schema(self, request=None, public=False): 
+        schema = super().get_schema(request, public) 
+        schema.schemes = ["http", "https"] 
+        return schema
+   
 schema_view = get_schema_view(openapi.Info(
     title="MANAGER_API",
     default_version='v1',
@@ -29,6 +36,7 @@ schema_view = get_schema_view(openapi.Info(
     license=openapi.License(name="BSD License"),
 ),
     public=True,
+    generator_class=BothHttpAndHttpsSchemaGenerator,
     permission_classes=(permissions.AllowAny,), )
 
 urlpatterns = [
